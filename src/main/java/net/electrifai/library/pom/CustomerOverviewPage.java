0e3150d149f1d931e1d2902aad8207522456eeb3
Naga Balaji Kumar <naga.kumar@electrifai.net>
2023-04-14 12:42:03 +0530

    CustomerOverviewPage added

--- src/main/java/net/electrifai/library/pom/CustomerOverviewPage.java
+++ src/main/java/net/electrifai/library/pom/CustomerOverviewPage.java
@@ -1,11 +1,12 @@
 package net.electrifai.library.pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CustomerOverviewPage extends HomePage{
 
-    
-
-
-
+    @FindBy(xpath = "//div[@class='customerOverview_title__o_i4H']/span")
+    WebElement myProductsHeadings;
 
 
 }
