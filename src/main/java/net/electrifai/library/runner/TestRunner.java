df27508950406428c9221e2c66571f0c3bffb6a4
Naga Balaji Kumar <naga.kumar@electrifai.net>
2023-05-03 11:09:14 +0000

    Update TestRunner.java

--- src/main/java/net/electrifai/library/runner/TestRunner.java
+++ src/main/java/net/electrifai/library/runner/TestRunner.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import net.electrifai.library.utils.BaseClass;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/feature",
+@CucumberOptions(features = "src/test/resources/features",
         glue = {"net/electrifai/setpdefinations", "net/electrifai/library"},
         monochrome = true,dryRun = false,snippets = CucumberOptions.SnippetType.CAMELCASE,plugin = {"pretty",
         "html:target/cucumber.html",
